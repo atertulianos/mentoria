5845a6b63cd833241cad1b68854b45b4ff0905af
Anderson Tertuliano <anderson.santos@medway.com.br>
2022-09-14 20:29:30 -0300

    add react boostrap e styled components

---
+++ src/compoments/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer(){
+    return(
+        <>
+            <footer class="bg-dark p-4 d-flex">
+                <address class="text-white text-start">
+                    Endereço: Rua QRO A Conjunto H
+                </address>
+            </footer>
+        </>
+    )
+}
---
+++ src/compoments/Footer/styles.ts
@@ -0,0 +1,3 @@
+import { styled } from 'styled-components';
+
+export class Container styled.div``;
